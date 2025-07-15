05c6dd9f36364930964d077c7736f9efa2b4bd13
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-15 14:58:15 +0100

    Set up cors in app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fruitsApp = express();
+const cors = require('cors');
 const fruitsRoutes = require("./routes/fruitsRoutes")
 const nutritionRoutes = require("./routes/nutritionRoutes")
 
 fruitsApp.get('/', (req, res) => {
    res.send('Hello Fruity!')
 })
+fruitsApp.use(cors());
 fruitsApp.use(express.json());
 fruitsApp.use('/fruits', fruitsRoutes)
 fruitsApp.use('/nutrition', nutritionRoutes)
