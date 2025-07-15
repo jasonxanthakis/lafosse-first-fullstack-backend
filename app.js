9353539e9618ef7f551ed1da94690f2069b1e8e6
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-15 15:50:16 +0100

    Third attempt to give CORS permissions

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const fruitsRoutes = require("./routes/fruitsRoutes")
 const nutritionRoutes = require("./routes/nutritionRoutes")
 
 fruitsApp.get('/', (req, res) => {
-   res.setHeader("Access-Control-Allow-Origin", "https://fruit-salad-builder.onrender.com/");
    res.send('Hello Fruity!')
 })
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://fruit-salad-builder.onrender.com');
+
+    // Pass to next layer of middleware
+    next();
+});
+
 fruitsApp.use(cors());
 fruitsApp.use(express.json());
 fruitsApp.use('/fruits', fruitsRoutes)
