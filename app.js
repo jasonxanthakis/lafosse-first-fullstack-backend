919811cb583fabe9fd01268dfca86ad334e4053b
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-15 15:53:14 +0100

    Fourth attempt to give CORS permissions

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ fruitsApp.get('/', (req, res) => {
 })
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
+fruitsApp.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://fruit-salad-builder.onrender.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
 
     // Pass to next layer of middleware
     next();
