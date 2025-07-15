44ab2c6c323d6991ae1ad634d6dca76188eb66c7
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-15 16:25:59 +0100

    Ninth attempt to give CORS permissions

--- controllers/fruitsController.js
+++ controllers/fruitsController.js
@@ -14,7 +14,7 @@ const showFruit = async (req, res) => {
 	
     try {
         const fruit = await FruitModel.getFruit(name);
-        res.status(200).send(fruit);
+        res.status(200).header('Access-Control-Allow-Origin', 'https://fruit-salad-builder.onrender.com').send(fruit);
     } catch(err) {
 	    res.status(404).send({error: err})
     }
